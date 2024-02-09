eb4bcccafec08138e91ca02a28cc6118783b7c86
Moisés Pastor <33151458+moisesPastor@users.noreply.github.com>
2024-02-09 12:45:12 +0100

    Update get_confMats_binary.py

--- get_confMats_binary.py
+++ get_confMats_binary.py
@@ -28,7 +28,7 @@ def test(model, htr_dataset, test_loader, device, numClasses, batch_size=20, con
         f_out = open(confMatrix, 'wb')
         
         # Main batch loop
-        for ((x, input_lengths),(y,target_lengths), bIdxs) in tqdm(test_loader, colour='cyan', colour='cyan', desc='Test'):
+        for ((x, input_lengths),(y,target_lengths), bIdxs) in tqdm(test_loader, colour='cyan', desc='Test'):
             #To GPU (or CPU)
             x = x.to(device)
             
